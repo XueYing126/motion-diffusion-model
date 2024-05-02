6b714bc421b9a00cb6f54e29652a961e53aeb07d
Ying Xue <yinxue@sip-ml.inf.ethz.ch>
2024-05-02 15:11:16 +0200

    01

--- eval/sample_test.py
+++ eval/sample_test.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     # sample all test
     text_dir = './dataset/HumanML3D/texts'
     split_file = './dataset/HumanML3D/test.txt'
-    save_folder = './dataset/HumanML3D/eval/sample_00'
+    save_folder = './dataset/HumanML3D/eval/sample_01'
     
     id_list = []
     with cs.open(split_file, 'r') as f:
